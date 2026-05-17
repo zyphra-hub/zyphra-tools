0017f2e18de3989f7abf0d26e2f5958e7ab699ff
ndrael.gh <zyphrahub@gmail.com>
2026-05-17 17:15:00 +0800

    Create help.py for support contact details
    Added help module with contact information.

---
+++ modules/help.py
@@ -0,0 +1,9 @@
+DESCRIPTION = "Contact support"
+def run():
+    print("""
+  ┌─ HELP / CONTACT
+  │ Email  : zyphrahub@gmail.com
+  │ GitHub : https://github.com/zyphra-hub/zyphra-tools
+  │
+  │ For bugs, suggestions, or collaboration.
+""")
