5e5ee09b4046ec99a35b4a3f3c881503f0a6446a
ndrael.gh <zyphrahub@gmail.com>
2026-05-17 17:33:34 +0800

    Update install.sh to include run instruction

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 echo "[*] Installing Zyphra Tools..."
 pkg update -y && pkg upgrade -y
 pkg install python -y
-pkg install git -y
 pip install --upgrade pip
 pip install -r requirements.txt
 chmod +x zyphra.py
